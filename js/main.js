7e95e1daf282f8c2a83c388ea22f3f59fd5e0c07
Yura Stryzhak <yuriy.stryzhak@cheitgroup.com>
2022-06-07 16:21:25 +0300

    Some new add

--- js/main.js
+++ js/main.js
@@ -4,17 +4,44 @@ const descField = document.querySelector('.js-desc');
 
 const list = document.querySelector('.js-list');
 
+let smallDesc = '';
 
 addBtn.addEventListener('click', function () {
 
-  let newData = `
-    <label>
-      <input type="checkbox" /><span class="js-note-name">${noteField.value}</span> <small class="js-note-desc"> ${descField.value} </small>
-      <button class="btn-more">...</button>
-      <!-- <i class="far fa-trash-alt"></i> -->
-    </label>
-  `;
+  if (!noteField.value.trim().length) {
+    noteField.value = '';
+    noteField.placeholder = 'This field must be filled';
+    noteField.classList += ' error';
+  } else {
 
-  list.innerHTML += newData;
+    if (descField.value.trim().length) {
+      smallDesc = `<small class="js-note-desc"> ${descField.value} </small>`;
+    }
 
+    let newData = `
+      <label>
+        <input type="checkbox" />
+        <span class="js-note-name">${noteField.value}</span> 
+        ${smallDesc}
+        <button class="btn-more">...</button>
+        <!-- <i class="far fa-trash-alt"></i> -->
+      </label>
+    `;
+
+    list.insertAdjacentHTML('afterbegin', newData);
+
+    noteField.value = '';
+    descField.value = '';
+    smallDesc = '';
+
+  }
+
+})
+
+noteField.addEventListener('focus', function () {
+  this.classList.remove('error');
+})
+
+noteField.addEventListener('blur', function () {
+  this.placeholder = 'Add your note...';
 })
